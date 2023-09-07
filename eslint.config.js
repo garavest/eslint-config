4266275f19988ea94de6471e021848f9c7056691
Seth Murphy <3085408+eagerestwolf@users.noreply.github.com>
2023-09-07 10:00:01 -0400

    refactor: changed project to have more standard layout
    NOTE: Preparing for TypeScript rewrite

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line import/no-useless-path-segments
-import { garavest } from "./index.js";
+import { garavest } from "./src/index.js";
 
 /** @type {import("eslint").Linter.FlatConfig} */
 export default garavest;
